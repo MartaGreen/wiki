004ad3dd0c14359ba9a520b17854775f133bba95
MartaGreen <iguana13131@gmail.com>
2022-06-11 12:33:21 +0400

    feat: add request status and loading icon

--- wikipedia/src/components/article/article.tsx
+++ wikipedia/src/components/article/article.tsx
@@ -1,37 +1,55 @@
 import React, { useEffect, useState } from "react";
+import styles from "./article.style";
 import {
   ICityRequest,
   IPage,
   IPageInfo,
 } from "../../interfaces/cityRequest.interface";
+
 import { getCityByString } from "../../requests/city.requests";
-import styles from "./article.style";
+import { REQUEST_STATUS } from "../../constants/request.constants";
+import loadinIcon from "../../assets/loadinIcon.svg";
 
 function Article({ searchedString }: { searchedString: string }) {
   const [content, setContent] = useState({} as IPageInfo);
+  const [status, setStatus] = useState(REQUEST_STATUS.pending);
   const classes = styles();
 
   useEffect(() => {
     (async function () {
+      setStatus(REQUEST_STATUS.pending);
       const cityRequestData: ICityRequest | null = await getCityByString(
         searchedString
       );
-      if (!cityRequestData || !cityRequestData.query) return;
-      const page: IPage = cityRequestData.query.pages;
-      const pageId: number = Number(Object.keys(page)[0]);
-      const pageInfo: IPageInfo = page[pageId];
-      setContent(pageInfo);
+
+      if (!cityRequestData) {
+        setStatus(REQUEST_STATUS.error);
+        return;
+      }
+      if (cityRequestData.query) {
+        setStatus(REQUEST_STATUS.success);
+
+        const page: IPage = cityRequestData.query.pages;
+        const pageId: number = Number(Object.keys(page)[0]);
+        const pageInfo: IPageInfo = page[pageId];
+        setContent(pageInfo);
+      }
     })();
   }, [searchedString]);
+
+  if (status === REQUEST_STATUS.pending) {
+    return <img src={loadinIcon} alt="loading" />;
+  }
+  if (status === REQUEST_STATUS.error) {
+    return <div>error</div>;
+  }
 
   return (
     <section className={classes.article}>
       <h2 className={classes.article__title}>{content.title}</h2>
-      {content.extract
-        ? content.extract.split("\n").map((content, index) => {
-            return <p key={index}>{content}</p>;
-          })
-        : ""}
+      {content.extract.split("\n").map((content, index) => {
+        return <p key={index}>{content}</p>;
+      })}
     </section>
   );
 }
---
+++ wikipedia/src/constants/request.constants.ts
@@ -0,0 +1,5 @@
+export const REQUEST_STATUS = {
+  pending: "pending",
+  success: "success",
+  error: "error",
+};
